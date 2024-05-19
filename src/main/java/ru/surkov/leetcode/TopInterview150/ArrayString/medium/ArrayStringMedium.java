c29374e51da66af0488c5217fad9c27f6660bc3b
artym <artym.surkov@mail.ru>
2024-05-19 21:50:51 +0500

    Leet code. Resolve - 189.Rotate Array.

--- src/main/java/ru/surkov/leetcode/TopInterview150/ArrayString/medium/ArrayStringMedium.java
+++ src/main/java/ru/surkov/leetcode/TopInterview150/ArrayString/medium/ArrayStringMedium.java
@@ -28,4 +28,25 @@ public class ArrayStringMedium {
     }
     return index;
   }
+
+  /**
+   * 189.Rotate Array.Given an integer array nums, rotate the array to the right by k steps, where k
+   * is non-negative.
+   */
+  public void rotate(int[] nums, int k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+
+  private void reverse(int[] nums, int left, int right) {
+    while (left < right) {
+      int temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+      right--;
+    }
+  }
 }
--- src/test/java/ru/surkov/leetcode/TopInterview150/ArrayString/medium/ArrayStringMediumTest.java
+++ src/test/java/ru/surkov/leetcode/TopInterview150/ArrayString/medium/ArrayStringMediumTest.java
@@ -2,6 +2,7 @@ package ru.surkov.leetcode.TopInterview150.ArrayString.medium;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -33,7 +34,30 @@ class ArrayStringMediumTest {
     public void case2() {
       int[] nums = new int[]{0, 0, 1, 1, 1, 1, 2, 3, 3};
       int k = arrayStringMedium.removeDuplicates(nums);
-     assertEquals(7, k);
+      assertEquals(7, k);
+    }
+  }
+
+  @Nested
+  @DisplayName("189.Rotate Array")
+  class RotateArray {
+
+    @Test
+    @DisplayName("Case 1")
+    public void case1() {
+      int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+      int k = 3;
+      arrayStringMedium.rotate(nums, k);
+      Assertions.assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, nums);
+    }
+
+    @Test
+    @DisplayName("Case 2")
+    public void case2() {
+      int[] nums = new int[]{-1, -100, 3, 99};
+      int k = 2;
+      arrayStringMedium.rotate(nums, k);
+      Assertions.assertArrayEquals(new int[]{3, 99, -1, -100}, nums);
     }
   }
 
